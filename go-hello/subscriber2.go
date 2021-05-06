41d995308ef367e1472e1d32841765922c516aed
harmands686 <33508578+harmands686@users.noreply.github.com>
2021-05-06 11:09:54 +0530

    Update and rename consumer2.go to subscriber2.go

--- go-hello/subscriber2.go
+++ go-hello/subscriber2.go
@@ -33,14 +33,14 @@ func main() {
 
 	msgs, err := ch.Consume(
 		q.Name, // queue
-		"consumer2",     // consumer
+		"subscriber2",     // consumer
 		true,   // auto-ack
 		false,  // exclusive
 		false,  // no-local
 		false,  // no-wait
 		nil,    // args
 	)
-	failOnError(err, "Failed to register a consumer2")
+	failOnError(err, "Failed to register a subscriber2")
 
 	for d := range msgs {
 		log.Printf("Received a message: %s", d.Body)
