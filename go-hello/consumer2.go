8932b97ec5c09d70bb945484600570b4d2304058
harmands686 <33508578+harmands686@users.noreply.github.com>
2021-05-06 10:55:35 +0530

    Create consumer2.go

---
+++ go-hello/consumer2.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+
+	"amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq-1.rabbitmq.rabbits.svc.cluster.local:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"HelloQueue", // name
+		true,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"consumer2",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer2")
+
+	for d := range msgs {
+		log.Printf("Received a message: %s", d.Body)
+	}
+}
